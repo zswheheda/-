5dde955d5de2a23cedce322e5948d8c6cae6e75d
zhoushengwang <291873236@qq.com>
2016-12-28 09:59:26 +0800

    权限控制完整版

---
+++ src/main/java/com/bgy/core/shiro/cache/ShiroCacheManager.java
@@ -0,0 +1,12 @@
+package com.bgy.core.shiro.cache;
+
+import org.apache.shiro.cache.Cache;
+
+
+public interface ShiroCacheManager {
+
+    <K, V> Cache<K, V> getCache(String name);
+
+    void destroy();
+
+}
